a554f28270abb1006e6b3fe94f09a9d1866dca33
Tim McCormack <tmccormack@brightcove.com>
2016-07-11 14:05:17 -0400

    black triangle: a graph is displayed

---
+++ app.js
@@ -0,0 +1,54 @@
+var app = {
+  descriptors: null,
+  descriptorsUrl: '/descriptors.json'
+};
+
+function collectElements() {
+  var elems = [];
+  // Mark node IDs as seen or not seen (but at least referenced) so we can
+  // fill in missing node IDs at the end.
+  var seenNodes = {};
+
+  $.each(app.descriptors.services, function(_i, svc) {
+    elems.push({
+      group: "nodes",
+      data: {id: svc.id}
+    });
+    seenNodes[svc.id] = true;
+
+    $.each(svc.dependencies, function(_j, dep) {
+      elems.push({
+        group: "edges",
+        data: {
+          source: svc.id,
+          target: dep.on
+        }
+      });
+      seenNodes[dep.on] = seenNodes[dep.on] || false;
+    });
+  });
+
+  // Fix up missing nodes and warn in console
+  $.each(seenNodes, function(id, defined) {
+    if (defined) return;
+    console.warn("Service referenced but not defined:", id);
+    elems.push({
+      group: "nodes",
+      data: {id: id},
+      classes: 'missing-ref'
+    });
+  });
+  return elems;
+}
+
+function init() {
+  $.get(app.descriptorsUrl, function recvinit(data, _status, _xhr) {
+    app.descriptors = data;
+    cytoscape({
+      container: document.getElementById('cytoscape-container'),
+      elements: collectElements()
+    });
+  });
+}
+
+$(init);
--- run.sh
+++ run.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
 set -eu
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 descriptors="$1"
 port="$2"
 
-localdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-build="$localdir/target"
+cp -- "$1" "$DIR/descriptors.json"
 
-[ -d "$build" ] || mkdir -- "$build"
-[ -d "$build/lib" ] || mkdir -- "$build/lib"
-[ -d "$build/data" ] || mkdir -- "$build/data"
-cp -t "$build" "$localdir/src/"*
-cp -t "$build/lib" "$localdir/vendor/"*
-cp -- "$descriptors" "$build/data/services.json"
-
-# Now that all relative paths have been resolved, feel free to change
-# working directory
-
-cd "$build"
 echo "Go to http://localhost:$port/index.html"
-python -mSimpleHTTPServer "$port"
+(cd -- "$DIR"; python -mSimpleHTTPServer "$port")
